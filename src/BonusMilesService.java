328b9ecb662669f081bd252615dc70eaa2a63581
Alex Vergizov <alvergizoff@gmail.com>
2022-07-24 22:52:40 +0300

    BonusMilesService corrected

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -11,9 +11,3 @@ public class BonusMilesService {
 
 
 
-
-    //int r = 20; //количество рублей, за которую начисляется 1 миля
-    //int miles; //количество начисленных миль за купленный билет
-    //int ticketCost = 858; //стоимость билета
-
-       // miles = ticketCost / r;
